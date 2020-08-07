dd3f9800ea403637fbb603cf874f2406599ebba1
Melinda Diaz <melindadiaz@me.com>
2020-08-07 18:53:38 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -49,13 +49,12 @@ end
 #####################################################
 
 def runner
-  
   welcome
-  card_total = initial_round
-  until card_total > 21
-    card_total = hit?(card_total)
-    display_card_total(card_total)
-  end
-  end_game(card_total)
+  initial_round
+  prompt_user
+  get_user_input
+  hit?(current_total)
+  
+  
 end
     

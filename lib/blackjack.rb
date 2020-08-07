3ea6f7e03031ebcbb39824feef31be2c54a1c73f
Melinda Diaz <melindadiaz@me.com>
2020-08-07 18:56:00 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,9 +31,9 @@ end
 def hit?(current_total)
   prompt_user
   get_user_input
-   if get_user_input == 's'
+   if get_user_input == s
      exit
-  elseif get_user_input == 'h'
+  elseif get_user_input == h
     deal_card
   else  invalid_command
   end
